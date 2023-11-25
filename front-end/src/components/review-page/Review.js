84925079f0939b443048096da68024de189e1cc2
drojaa <119087348+drojaa@users.noreply.github.com>
2023-11-24 21:44:24 -0500

    This is the js file for the Review Submission Page

---
+++ front-end/src/components/review-page/Review.js
@@ -0,0 +1,13 @@
+import './Review.css';
+import Form from '../form-page/Form';
+
+function Review() {
+    return (
+      <div className='review-div'>
+        <Form />  
+      </div>  
+       
+    )
+  }
+
+  export default Review
