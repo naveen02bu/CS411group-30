b6fd90de9b7f171896f06532a1459d520bf07f7d
Derinell Rojas <finethang@nat-wireless-guest-reg-153-50.bu.edu>
2023-12-03 02:10:18 -0500

    Made the form a pop-up

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,9 +1,22 @@
+import React, { useState } from 'react';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
 import bookifyIcon from '../../images/bookify-icon.png';
 import Nav from '../nav/Nav';
+import Form from '../form-page/Form'; // Import the Form component
+
 
 function Header() { 
+  const [isFormModalOpen, setIsFormModalOpen] = useState(false);
+
+  const openFormModal = () => {
+    setIsFormModalOpen(true);
+  };
+
+  const closeFormModal = () => {
+    setIsFormModalOpen(false);
+  };
+
   return (
     <div className='header-div'>
       <div className='header-left'>
@@ -12,7 +25,19 @@ function Header() {
       </div>
       <Nav />
       <div className='header-right' >
-        <NavLink className='review-link' to='/review'>Write a review</NavLink>
+        <button onClick={openFormModal}>Write a review</button>
+        {isFormModalOpen && (
+          <div className="form-modal">
+            <div className="form-modal-content">
+              {/* Close button for the form modal */}
+              <span className="close" onClick={closeFormModal}>&times;</span>
+
+              {/* Render the Form component */}
+              <Form />
+            </div>
+          </div>
+        )}
+
         <NavLink className='log-out-link' to='/'>Log out</NavLink>
       </div>
     </div>
