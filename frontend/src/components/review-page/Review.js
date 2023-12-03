3a25ed2124a6ac74334b06a0af3970729c75026c
Derinell Rojas <finethang@nat-wireless-guest-reg-153-50.bu.edu>
2023-12-03 01:22:47 -0500

    Added the Footer to Review Page

--- frontend/src/components/review-page/Review.js
+++ frontend/src/components/review-page/Review.js
@@ -1,13 +1,14 @@
 import './Review.css';
 import Form from '../form-page/Form';
+import Header from '../header/Header'
+import Footer from '../footer/Footer';
 
 function Review() {
     return (
       <div className='review-div'>
-        <Form />  
-      </div>  
-       
+        <Form /> 
+        <Footer /> 
+      </div>       
     )
   }
-
   export default Review
